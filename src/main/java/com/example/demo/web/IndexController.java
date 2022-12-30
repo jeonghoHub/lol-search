73fa5d224667bdf9edac8cf5bf95097e5ce47996
jeonghoHub <tjsrsdqq4@gmail.com>
2022-12-30 13:44:40 +0900

    feat : 티어 이미지&불러오기 기능 추가

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
+public class IndexController {
 
     @GetMapping("/")
     public String index() {
