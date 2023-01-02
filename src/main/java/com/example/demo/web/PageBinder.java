73901c5af6a21d8e6fa89229d5247acf7498a159
junnyJun <chbe5082@naver.com>
2023-01-02 10:34:28 +0900

    refactor(Config) : pageBinding

---
+++ src/main/java/com/example/demo/web/PageBinder.java
@@ -0,0 +1,18 @@
+package com.example.demo.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PageBinder implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addStatusController("/404", HttpStatus.NOT_FOUND);
+        registry.addStatusController("/500", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
